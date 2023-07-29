0b8d7cd6e23b16b750272710f6cac99eac79b9c3
Tonalli RL <t3rodrig@users.noreply.github.com>
2023-07-28 22:33:19 -0600

    # 2 - Making Panels

--- code/main.py
+++ code/main.py
@@ -1,6 +1,11 @@
 from settings import *
 from sys import exit
 
+# components
+from game import Game
+from score import Score
+from preview import Preview
+
 class Main:
     def __init__(self):
         
@@ -9,6 +14,11 @@ class Main:
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         self.clock = pygame.time.Clock()
         pygame.display.set_caption('Tetris')
+
+        # components
+        self.game = Game()
+        self.score = Score()
+        self.preview = Preview()
 
     def run(self):
         while True:
@@ -19,6 +29,11 @@ class Main:
 
             # display
             self.display_surface.fill(GRAY)
+
+            # components
+            self.game.run()
+            self.score.run()
+            self.preview.run()
 
             # update the game
             pygame.display.update()
